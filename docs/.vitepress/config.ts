3613b8cc03188ca5d3eaefb45eec2cd412a0591e
aotianlong <aotianlong@gmail.com>
2022-12-05 16:11:37 +0800

    save

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,17 @@
+export default {
+  title: 'AoTianlong的自媒体',
+  description: '这是一个傲天龙的自媒体网站',
+  themeConfig: {
+    nav: [
+      { text: 'Guide', link: '/guide' },
+      {
+        text: 'Dropdown Menu',
+        items: [
+          { text: 'Item A', link: '/item-1' },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' }
+        ]
+      }
+    ]
+  }
+}

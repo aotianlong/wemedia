0048daec446e3183d8276f8d890b0b0f8816b30c
aotianlong <aotianlong@gmail.com>
2022-12-05 18:13:11 +0800

    使用auto generate sidebar, 去掉.开头的目录

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,13 +1,31 @@
-import { getSidebar } from 'vitepress-plugin-auto-sidebar'
+import { getSidebar } from 'vitepress-plugin-auto-sidebar';
 
-const sidebar = getSidebar({ contentRoot: '/docs', contentDirs: ['life'], collapsible: true, collapsed: true });
+const sidebar = getSidebar({ contentRoot: 'docs', contentDirs: null, collapsible: true, collapsed: true });
 
+// 生成的连接有bug，没有包含目录名称
+const fixlink = (items = []) =>{
+  items = items.filter((item) => {
+    return !item.text.startsWith('.');
+  })
+  items.forEach((item) => {
+    if (item.link) {
+      item.link = item.link.replace('//', '/');
+    }
+    // item.link = root + item.link;
+    if(item.items) {
+      fixlink(item.items)
+    }
+  })
+  return items;
+}
+
+const fixedSidebar = fixlink(sidebar);
 
 export default {
   title: 'AoTianlong的自媒体',
   description: '这是一个傲天龙的自媒体网站',
   themeConfig: {
-    sidebar,
+    sidebar: fixedSidebar,
     nav: [
       // { text: 'Guide', link: '/guide' },
       {
